18712a56bd13b7a077653a953cbfff4e2c5044fc
Puneet Saini <puneet29saini@gmail.com>
2018-04-02 18:09:00 +0530

    2nd April

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * IMPORTANT: Make sure you are using the correct package name. 
+ * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
  * package com.example.android.justjava
  * If you get an error when copying this code into Android studio, update it to match teh package name found
@@ -9,12 +9,21 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.text.NumberFormat;
+
+import javax.security.auth.Subject;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,52 +36,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
-    int quantity=0;
+    int quantity = 1;
+
     public void submitOrder(View view) {
-        int price= calculatePrice();
-        String summary= createOrderSummary(price);
-        displayMessage(summary);
+        boolean chocoChecked;
+        boolean whippedCreamChecked;
+
+        EditText nameTextField = findViewById(R.id.name_text_field);
+        String name = nameTextField.getText().toString();
+        CheckBox whippedCream = findViewById(R.id.whippedCream);
+        CheckBox chocolate = findViewById(R.id.chocolate);
+        whippedCreamChecked = whippedCream.isChecked();
+        chocoChecked = chocolate.isChecked();
+
+        int price = calculatePrice(chocoChecked, whippedCreamChecked);
+        String summary = createOrderSummary(price, name, chocoChecked, whippedCreamChecked);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setType("text/plain");
+        intent.setData(Uri.parse("mailto: puneetsainirockz@gmail.com"));
+        intent.putExtra(Intent.EXTRA_TEXT, summary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
-    /**
-     * Calculates the price of the order.
-     */
-    private int calculatePrice() {
-        return (quantity * 5);
+    private int calculatePrice(boolean chocoChecked, boolean whippedCreamChecked) {
+        int price=5;
+        if(chocoChecked && whippedCreamChecked){
+            price+=3;
+        }
+        else if (chocoChecked){
+            price+=2;
+        }
+        else if (whippedCreamChecked){
+            price++;
+        }
+        return (quantity * price);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
     private void displayQuantity(int num) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + num);
     }
 
-
     public void increment(View view) {
+        if(quantity>=100) {
+            Toast.makeText(this, getString(R.string.max_warning), Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity++;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity<=1) {
+            Toast.makeText(this, getString(R.string.min_warning), Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
         displayQuantity(quantity);
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
-    private String createOrderSummary(int price){
-        return("Name: Kaptain Kunal\n" + "Quantity: " + quantity + "\nTotal: $" + price + "\nThank you!");
+    private String createOrderSummary(int price, String name, boolean chocoChecked, boolean whippedCreamChecked) {
+        return (getString(R.string.order_name, name) +
+                getString(R.string.whipped_cream_check, whippedCreamChecked) +
+                getString(R.string.chocolate_check, chocoChecked) +
+                "\n" + getString(R.string.quantity) + ": " + quantity +
+                getString(R.string.price) + price +
+                getString(R.string.thank_you));
     }
 
 }
